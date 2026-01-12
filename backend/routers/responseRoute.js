84d6be41799c3fa50f879a0140c1cdbf15dfb30b
Nithya <nithyasundaram31@gmail.com>
2026-01-12 22:39:08 +0530

    add getAllResponse rute

--- backend/routers/responseRoute.js
+++ backend/routers/responseRoute.js
@@ -1,11 +1,12 @@
 const express=require("express")
-const { createResponse, getSurveyResult, getResponses, getSurveySummary } = require("../controllers/responseController");
+const { createResponse, getSurveyResult, getResponses, getSurveySummary, getAllResponse } = require("../controllers/responseController");
 const authenticate = require("../middleware/auth");
 
 const responseRoute=express.Router()
 
 
 responseRoute.post('/',authenticate(),createResponse);
+responseRoute.get('/',getAllResponse)
 responseRoute.get('/submitted',authenticate(),getResponses)
 responseRoute.get('/summary',getSurveySummary)
 responseRoute.get('/:id',getSurveyResult)
